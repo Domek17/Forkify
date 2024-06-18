45319ad282afeb25d51f59f2e81792ac1654bb16
Domagoj <sasukeboyka@hotmail.com>
2024-06-18 17:16:04 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `5be7d37a-6c03-45b5-88fb-d883cefef4ec`;
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/view.js
@@ -0,0 +1,97 @@
+import icons from 'url:../../img/icons.svg';
+
+export default class View {
+  _data;
+
+  /**
+   * Render the received object to the DOM
+   * @param {Object || Object[]} data The data to be rendered (e.g.recipe)
+   * @returns {undefined}
+   * @this {Object} View instance
+   * @author Domagoj Kanaet
+   * @todo Finish implementation
+   */
+  render(data) {
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderErrorMessage();
+
+    this._data = data;
+    const markup = this._generateMarkup();
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  update(data) {
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+
+      // Updates changed TEXT
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        curEl.textContent = newEl.textContent;
+      }
+
+      // Updates changed ATTRIBUTES
+      if (!newEl.isEqualNode(curEl)) {
+        Array.from(newEl.attributes).forEach(attr =>
+          curEl.setAttribute(attr.name, attr.value)
+        );
+      }
+    });
+  }
+
+  _clear() {
+    this._parentElement.innerHTML = '';
+  }
+
+  renderSpinner() {
+    const spinnerTemplate = `
+      <div class="spinner">
+        <svg>
+          <use href="${icons}#icon-loader"></use>
+        </svg>
+      </div>
+    `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', spinnerTemplate);
+  }
+
+  renderErrorMessage(message = this._errorMessage) {
+    const errorMessageTemplate = `
+      <div class="error">
+        <div>
+          <svg>
+            <use href="${icons}#icon-alert-triangle"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', errorMessageTemplate);
+  }
+
+  renderMessage(message = this._successMessage) {
+    const errorMessageTemplate = `
+      <div class="message">
+        <div>
+          <svg>
+            <use href="${icons}#icon-smile"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', errorMessageTemplate);
+  }
+}

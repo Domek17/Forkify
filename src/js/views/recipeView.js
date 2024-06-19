4f0af3d56ba5e5bd3af17ab9afe3db15ee11f175
Domagoj <sasukeboyka@hotmail.com>
2024-06-19 11:56:51 +0200

    Removed Fractional library, added fracty library

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import icons from 'url:../../img/icons.svg';
-import { fracty } from 'fracty';
+import fracty from 'fracty';
 import View from './view';
 
 class RecipeView extends View {
@@ -127,7 +127,7 @@ class RecipeView extends View {
           <use href="${icons}#icon-check"></use>
         </svg>
         <div class="recipe__quantity">${
-          ing.quantity ? fracty(ing.quantity) : ''
+          ing.quantity ? fracty(ing.quantity).toString() : ''
         }</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
